4948cf9159e69bf9b71a127c96f10b988e50c4e5
mfish33 <maxmfishernj@gmail.com>
2022-04-28 12:20:29 -0700

    lifetimes!

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,47 @@
+use std::{borrow::Borrow, rc::Rc};
+
 use lexpr::{self};
 
 #[derive(Clone, Debug, PartialEq)]
-enum ExprC {
+enum ExprC<'a> {
     IfC {
-        condition: Box<ExprC>,
-        then: Box<ExprC>,
-        els: Box<ExprC>,
+        condition: Box<ExprC<'a>>,
+        then: Box<ExprC<'a>>,
+        els: Box<ExprC<'a>>,
     },
     NumC(f64),
-    IdC(String),
+    IdC(&'a str),
     AppC {
-        fun: Box<ExprC>,
-        args: Vec<ExprC>,
+        fun: Box<ExprC<'a>>,
+        args: Vec<ExprC<'a>>,
     },
     CloC {
-        parameters: Vec<String>,
-        body: Box<ExprC>,
+        parameters: Vec<&'a str>,
+        body: Box<ExprC<'a>>,
     },
-    StringC(String),
+    StringC(&'a str),
 }
 
 #[derive(Clone, Debug, PartialEq)]
-enum Value {
+enum Value<'a> {
     NumV(f64),
     CloV {
-        parameters: Vec<String>,
-        body: ExprC,
-        environment: Environment,
+        parameters: Vec<&'a str>,
+        body: ExprC<'a>,
+        environment: Environment<'a>,
     },
-    StringV(String),
+    StringV(&'a str),
     BoolV(bool),
-    Intrinsic(Intrinsic),
+    Intrinsic(Intrinsic<'a>),
 }
 
 #[derive(Clone, Debug, PartialEq)]
-enum Intrinsic {
-    Binary(fn(Value, Value) -> Value),
-    Unary(fn(Value) -> Value),
+enum Intrinsic<'a> {
+    Binary(fn(Rc<Value<'a>>, Rc<Value<'a>>) -> Rc<Value<'a>>),
+    Unary(fn(Rc<Value<'a>>) -> Rc<Value<'a>>),
 }
 
-impl Value {
+impl<'a> Value<'a> {
     fn as_bool(&self) -> Option<bool> {
         match self {
             &Value::BoolV(bool) => Some(bool),
@@ -48,25 +50,25 @@ impl Value {
     }
 }
 
-impl From<f64> for Value {
+impl<'a> From<f64> for Value<'a> {
     fn from(num: f64) -> Self {
         Value::NumV(num)
     }
 }
 
-impl From<bool> for Value {
+impl<'a> From<bool> for Value<'a> {
     fn from(bool: bool) -> Self {
         Value::BoolV(bool)
     }
 }
 
 #[derive(Clone, Debug, PartialEq)]
-struct BindingV {
-    pub from: String,
-    pub to: Value,
+struct BindingV<'a> {
+    pub from: &'a str,
+    pub to: Rc<Value<'a>>,
 }
 
-type Environment = Vec<BindingV>;
+type Environment<'a> = Vec<BindingV<'a>>;
 
 fn main() {
     let source = "(var (
@@ -76,13 +78,25 @@ fn main() {
     println!("Fibonacci of 17 is: {:?}", result);
 }
 
-fn parse(sexp: &lexpr::Value) -> ExprC {
+trait LexprConsExtensions {
+    fn to_shared_vec(&self) -> Vec<&lexpr::Value>;
+}
+
+impl LexprConsExtensions for lexpr::Cons {
+    fn to_shared_vec(&self) -> Vec<&lexpr::Value> {
+        self.list_iter().collect()
+    }
+}
+
+fn parse<'a>(sexp: &'a lexpr::Value) -> ExprC<'a> {
     match sexp {
-        lexpr::Value::Symbol(id) => ExprC::IdC(id.to_string()),
+        lexpr::Value::Symbol(id) => ExprC::IdC(id),
         lexpr::Value::Number(num) => ExprC::NumC(num.as_f64().unwrap()),
-        lexpr::Value::String(string) => ExprC::StringC(string.to_string()),
+        lexpr::Value::String(string) => ExprC::StringC(string),
         lexpr::Value::Cons(cons) => {
-            let tokens = cons.to_vec().0;
+            let tokens = cons.to_shared_vec();
+            println!("tokens: len:{} {:?}", tokens.len(), tokens);
+            println!("cons: {:?}", cons);
             match &tokens[..] {
                 [lexpr::Value::Symbol(if_token), condition, then, els] if &**if_token == "if" => {
                     ExprC::IfC {
@@ -95,16 +109,14 @@ fn parse(sexp: &lexpr::Value) -> ExprC {
                     if &**var == "var" && &**in_keyword == "in" =>
                 {
                     let bindings: Vec<_> = bindings_any
-                        .to_vec()
-                        .0
+                        .to_shared_vec()
                         .iter()
                         .map(|cons_any| {
                             parse_binding(
                                 &cons_any
                                     .as_cons()
                                     .expect("unexpected binding syntax")
-                                    .to_vec()
-                                    .0,
+                                    .to_shared_vec(),
                             )
                         })
                         .collect();
@@ -120,7 +132,6 @@ fn parse(sexp: &lexpr::Value) -> ExprC {
                                 symbol_any
                                     .as_symbol()
                                     .expect("Params and name are strings")
-                                    .to_owned()
                             })
                             .collect(),
                     }
@@ -136,15 +147,15 @@ fn parse(sexp: &lexpr::Value) -> ExprC {
     }
 }
 
-fn create_binding_chain(
-    bindings: &[(String, ExprC)],
-    last: &lexpr::Value,
-) -> Box<ExprC> {
+fn create_binding_chain<'a>(
+    bindings: &[(&'a str, ExprC<'a>)],
+    last: &'a lexpr::Value,
+) -> Box<ExprC<'a>> {
     match &bindings[..] {
         [] => Box::new(parse(last)),
         [(from, to), rst @ ..] => Box::new(ExprC::AppC {
             fun: Box::new(ExprC::CloC {
-                parameters: vec![from.clone()],
+                parameters: vec![from],
                 body: create_binding_chain(rst, last),
             }),
             args: vec![to.clone()],
@@ -152,35 +163,35 @@ fn create_binding_chain(
     }
 }
 
-fn parse_binding(tokens: &Vec<lexpr::Value>) -> (String, ExprC) {
+fn parse_binding<'a>(tokens: &Vec<&'a lexpr::Value>) -> (&'a str, ExprC<'a>) {
     match &tokens[..] {
         [lexpr::Value::Symbol(from), lexpr::Value::Symbol(equals), to] if &**equals == "=" => {
-            (from.to_string(), parse(to))
+            (from, parse(to))
         }
         _ => panic!("`parse_binding` incorrect syntax for binding {:?}", tokens),
     }
 }
 
-fn environment_lookup(id: &str, env: &Environment) -> Value {
+fn environment_lookup<'a, 'b>(id: &str, env: &'b Environment<'a>) -> Rc<Value<'a>> {
     for binding in env.iter().rev() {
         if binding.from == id {
-            return binding.to.clone();
+            return Rc::clone(&binding.to);
         }
     }
     panic!("JILI unbound identifier: {}", id);
 }
 
-fn interp(exp: &ExprC, env: &Environment) -> Value {
+fn interp<'a>(exp: &ExprC<'a>, env: Environment<'a>) -> Rc<Value<'a>> {
     match exp {
-        ExprC::NumC(num) => Value::NumV(*num),
-        ExprC::StringC(string) => Value::StringV(string.clone()),
+        ExprC::NumC(num) => Rc::new(Value::NumV(*num)),
+        ExprC::StringC(string) => Rc::new(Value::StringV(string)),
         ExprC::IdC(id) => environment_lookup(&id, &env),
         ExprC::IfC {
             condition,
             then,
             els,
         } => {
-            if interp(condition, env)
+            if interp(condition, env.clone())
                 .as_bool()
                 .expect("Conditional check must be a boolean")
             {
@@ -189,21 +200,21 @@ fn interp(exp: &ExprC, env: &Environment) -> Value {
                 interp(els, env)
             }
         }
-        ExprC::CloC { parameters, body } => Value::CloV {
+        ExprC::CloC { parameters, body } => Rc::new(Value::CloV {
             body: *body.clone(),
             parameters: parameters.clone(),
             environment: env.clone(),
-        },
+        }),
         ExprC::AppC { fun, args } => {
             let value = match &**fun {
                 ExprC::IdC(name) => environment_lookup(&name, &env),
-                fn_exp => interp(&fn_exp, env),
+                fn_exp => interp(&fn_exp, env.clone()),
             };
-            match value {
+            match value.borrow() {
                 Value::CloV {
                     parameters,
                     body,
-                    environment:clo_env,
+                    environment: clo_env,
                 } => {
                     if parameters.len() != args.len() {
                         panic!(
@@ -215,14 +226,19 @@ fn interp(exp: &ExprC, env: &Environment) -> Value {
                     next_env.extend(
                         parameters
                             .into_iter()
-                            .zip(args.into_iter().map(|arg| interp(arg, env)))
-                            .map(|(from, to)| BindingV { from, to }),
+                            .zip(args.into_iter().map(|arg| interp(arg, env.clone())))
+                            .map(|(from, to)| BindingV {
+                                from,
+                                to,
+                            }),
                     );
 
-                    interp(&body, &next_env)
+                    println!("next env: {:?}", next_env);
+
+                    interp(&body, next_env)
                 }
                 Value::Intrinsic(Intrinsic::Binary(binary_op)) => match &args[..] {
-                    [left, right] => binary_op(interp(left, env), interp(right, env)),
+                    [left, right] => binary_op(interp(left, env.clone()), interp(right, env)),
                     _ => panic!(
                         "Can not apply binary op intrinsic without exactly 2 arguments {:?}",
                         args
@@ -241,7 +257,7 @@ fn interp(exp: &ExprC, env: &Environment) -> Value {
     }
 }
 
-fn serialize(value: Value) -> String {
+fn serialize(value: &Value) -> String {
     match value {
         Value::Intrinsic(_) => "#<primop>".into(),
         Value::CloV {
@@ -251,29 +267,31 @@ fn serialize(value: Value) -> String {
         } => "#<procedure>".into(),
         Value::BoolV(bool) => bool.to_string(),
         Value::NumV(num) => num.to_string(),
-        Value::StringV(string) => string,
+        Value::StringV(string) => string.to_string(),
     }
 }
 
 macro_rules! jili_binary_arith {
     ($op:tt) => {
         BindingV{
-            from: stringify!($op).to_string(),
-            to: Value::Intrinsic(Intrinsic::Binary(|left:Value, right: Value| {
-                match(left,right) {
-                    (Value::NumV(left), Value::NumV(right)) => (left $op right).into(),
+            from: stringify!($op),
+            to: Rc::new(Value::Intrinsic(Intrinsic::Binary(|left, right| {
+                match(left.borrow(),right.borrow()) {
+                    (Value::NumV(left), Value::NumV(right)) => Rc::new((left $op right).into()),
                     _ => panic!("Intrinsic functions require numeric arguments")
                 }
-        }))}
+        })))}
     }
 }
 
-fn jili_error(value: Value) -> Value {
-    panic!("JILI user-error got: {}", serialize(value))
+fn jili_error<'a>(value: Rc<Value>) -> Rc<Value<'a>> {
+    panic!("JILI user-error got: {}", serialize(&value))
 }
 
 fn top_interp(source: &str) -> String {
-    let prog = parse(&lexpr::from_str(&source).unwrap());
+    let sexp = lexpr::from_str(&source).unwrap();
+    let prog = parse(&sexp);
+    println!("prog: {:?}", prog);
     let base_env: Environment = vec![
         jili_binary_arith!(+),
         jili_binary_arith!(-),
@@ -281,25 +299,27 @@ fn top_interp(source: &str) -> String {
         jili_binary_arith!(/),
         jili_binary_arith!(<=),
         BindingV {
-            from: "equals?".into(),
-            to: Value::Intrinsic(Intrinsic::Binary(|a, b| (a == b).into())),
+            from: "equals?",
+            to: Rc::new(Value::Intrinsic(Intrinsic::Binary(|a, b| {
+                Rc::new(Value::BoolV(a == b))
+            }))),
         },
         BindingV {
-            from: "error".into(),
-            to: Value::Intrinsic(Intrinsic::Unary(jili_error)),
+            from: "error",
+            to: Rc::new(Value::Intrinsic(Intrinsic::Unary(jili_error))),
         },
         BindingV {
-            from: "true".into(),
-            to: Value::BoolV(true),
+            from: "true",
+            to: Rc::new(Value::BoolV(true)),
         },
         BindingV {
-            from: "false".into(),
-            to: Value::BoolV(false),
+            from: "false",
+            to: Rc::new(Value::BoolV(false)),
         },
     ];
 
-    let result = interp(&prog, &base_env);
-    serialize(result)
+    let result = interp(&prog, base_env);
+    serialize(&result)
 }
 
 #[cfg(test)]
