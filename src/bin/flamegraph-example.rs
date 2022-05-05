1d36fe451d2f255989d55f1ae3d16ffa4f9ce13b
mfish33 <maxmfishernj@gmail.com>
2022-05-04 18:59:56 -0700

    use nom as a parser

--- src/bin/flamegraph-example.rs
+++ src/bin/flamegraph-example.rs
@@ -2,11 +2,17 @@ use jili_rs::top_interp;
 
 fn main() {
     let x = 25;
+    // let source = format!(
+    //     "(var (
+    //     (fib = (x fib => (if (<= x 1) x (+ (fib (- x 1) fib) (fib (- x 2) fib)))))
+    // ) in (fib {} fib))",
+    //     x
+    // );
     let source = format!(
-        "(var (
-        (fib = (x fib => (if (<= x 1) x (+ (fib (- x 1) fib) (fib (- x 2) fib)))))
-    ) in (fib {} fib))",
+        "((fib =>
+            (fib {} fib)
+        ) (x fib => (if (<= x 1) x (+ (fib (- x 1) fib) (fib (- x 2) fib)))))",
         x
     );
-    top_interp(&source);
+    println!("{}", top_interp(&source));
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,14 @@
 use std::{borrow::Borrow, rc::Rc, time::Instant};
 
-use lexpr::{self};
+use nom::{
+    branch::alt,
+    bytes::complete::{tag, take_while, take_while1},
+    character::complete::{char, multispace0},
+    multi::{many0, many1},
+    number::complete::double,
+    sequence::{delimited, tuple},
+    IResult,
+};
 
 #[derive(Clone, Debug, PartialEq)]
 enum ExprC<'a> {
@@ -73,7 +81,7 @@ type Environment<'a> = Rc<Cons<BindingV<'a>>>;
 #[derive(Clone, Debug, PartialEq)]
 enum Cons<T> {
     Empty,
-    Some{ value:T, next: Rc<Cons<T>> }
+    Some { value: T, next: Rc<Cons<T>> },
 }
 
 impl<T> Cons<T> {
@@ -91,13 +99,16 @@ impl<T> Cons<T> {
 }
 
 trait ConsFunctionality<T> {
-    fn insert(&self, value:T) -> Self;
+    fn insert(&self, value: T) -> Self;
     fn extend(&self, it: impl Iterator<Item = T>) -> Self;
 }
 
-impl<T>ConsFunctionality<T> for Rc<Cons<T>> {
+impl<T> ConsFunctionality<T> for Rc<Cons<T>> {
     fn insert(&self, value: T) -> Self {
-        Rc::new(Cons::Some { value, next: Rc::clone(self) })
+        Rc::new(Cons::Some {
+            value,
+            next: Rc::clone(self),
+        })
     }
 
     fn extend(&self, it: impl Iterator<Item = T>) -> Self {
@@ -109,103 +120,137 @@ impl<T>ConsFunctionality<T> for Rc<Cons<T>> {
     }
 }
 
-trait LexprConsExtensions {
-    fn to_shared_vec(&self) -> Vec<&lexpr::Value>;
+fn parse_exprc(input: &str) -> IResult<&str, ExprC> {
+    delimited(
+        multispace0,
+        alt((
+            parse_cloc,
+            parse_ifc,
+            parse_var,
+            parse_appc,
+            parse_stringc,
+            parse_numc,
+            parse_idc,
+        )),
+        multispace0,
+    )(input)
 }
 
-impl LexprConsExtensions for lexpr::Cons {
-    fn to_shared_vec(&self) -> Vec<&lexpr::Value> {
-        self.list_iter().collect()
+fn parse_numc(input: &str) -> IResult<&str, ExprC> {
+    let (rest, result) = double(input)?;
+    Ok((rest, ExprC::NumC(result)))
+}
+
+fn parse_idc(input: &str) -> IResult<&str, ExprC> {
+    parse_legal_id(input).map(|(rest, res)| (rest, ExprC::IdC(res)))
+}
+
+fn parse_stringc(input: &str) -> IResult<&str, ExprC> {
+    delimited(char('"'), take_while(|char| char != '"'), char('"'))(input)
+        .map(|(rest, res)| (rest, ExprC::StringC(res)))
+}
+
+fn parse_legal_id(input: &str) -> IResult<&str, &str> {
+    let result =
+        take_while1(|char| !char::is_whitespace(char) && char != '(' && char != ')')(input)?;
+    if result.1 == "var" || result.1 == "=>" || result.1 == "if" {
+        Err(nom::Err::Error(nom::error::Error::new(
+            input,
+            nom::error::ErrorKind::Verify,
+        )))
+    } else {
+        Ok(result)
     }
 }
 
-fn parse(sexp: &lexpr::Value) -> ExprC<'_> {
-    match sexp {
-        lexpr::Value::Symbol(id) => ExprC::IdC(id),
-        lexpr::Value::Number(num) => ExprC::NumC(num.as_f64().unwrap()),
-        lexpr::Value::String(string) => ExprC::StringC(string),
-        lexpr::Value::Cons(cons) => {
-            let tokens = cons.to_shared_vec();
-            match &tokens[..] {
-                [lexpr::Value::Symbol(if_token), condition, then, els] if &**if_token == "if" => {
-                    ExprC::IfC {
-                        condition: Box::new(parse(condition)),
-                        then: Box::new(parse(then)),
-                        els: Box::new(parse(els)),
-                    }
-                }
-                [lexpr::Value::Symbol(var), lexpr::Value::Cons(bindings_any), lexpr::Value::Symbol(in_keyword), body]
-                    if &**var == "var" && &**in_keyword == "in" =>
-                {
-                    let bindings: Vec<_> = bindings_any
-                        .to_shared_vec()
-                        .iter()
-                        .map(|cons_any| {
-                            parse_binding(
-                                &cons_any
-                                    .as_cons()
-                                    .expect("unexpected binding syntax")
-                                    .to_shared_vec(),
-                            )
-                        })
-                        .collect();
-
-                    *create_binding_chain(&bindings, body)
-                }
-                [params @ .., lexpr::Value::Symbol(fn_def), body] if &**fn_def == "=>" => {
-                    ExprC::CloC {
-                        body: Box::new(parse(body)),
-                        parameters: params
-                            .iter()
-                            .map(|symbol_any| {
-                                symbol_any.as_symbol().expect("Params and name are strings")
-                            })
-                            .collect(),
-                    }
-                }
-                [fun, args @ ..] => ExprC::AppC {
-                    fun: Box::new(parse(fun)),
-                    args: args.iter().map(|sexp| parse(sexp)).collect(),
-                },
-                _ => panic!("`parse` Unexpected syntax while parsing: {}", sexp),
-            }
-        }
-        _ => panic!("`parse` Unexpected syntax while parsing: {}", sexp),
-    }
+fn parse_appc(input: &str) -> IResult<&str, ExprC> {
+    let (rest, mut result) = delimited(tag("("), many1(parse_exprc), tag(")"))(input)?;
+    let fun = Box::new(result.remove(0));
+    Ok((rest, ExprC::AppC { fun, args: result }))
 }
 
-fn create_binding_chain<'a>(
-    bindings: &[(&'a str, ExprC<'a>)],
-    last: &'a lexpr::Value,
-) -> Box<ExprC<'a>> {
-    match bindings {
-        [] => Box::new(parse(last)),
-        [(from, to), rst @ ..] => Box::new(ExprC::AppC {
-            fun: Box::new(ExprC::CloC {
-                parameters: vec![from],
-                body: create_binding_chain(rst, last),
-            }),
-            args: vec![to.clone()],
+fn parse_ifc(input: &str) -> IResult<&str, ExprC> {
+    let (rest, (_, _, _, condition, then, els, _, _)) = tuple((
+        tag("("),
+        multispace0,
+        tag("if"),
+        parse_exprc,
+        parse_exprc,
+        parse_exprc,
+        multispace0,
+        tag(")"),
+    ))(input)?;
+    Ok((
+        rest,
+        ExprC::IfC {
+            condition: Box::new(condition),
+            then: Box::new(then),
+            els: Box::new(els),
+        },
+    ))
+}
+
+fn parse_var(input: &str) -> IResult<&str, ExprC> {
+    let (rest, (_, _, _, bindings, _, _, _, body, _)) = tuple((
+        char('('),
+        delimited(multispace0, tag("var"), multispace0),
+        char('('),
+        many1(delimited(multispace0, parse_binding, multispace0)),
+        char(')'),
+        multispace0,
+        tag("in"),
+        delimited(multispace0, parse_exprc, multispace0),
+        char(')'),
+    ))(input)?;
+
+    let generated_exprc = ExprC::AppC {
+        fun: Box::new(ExprC::CloC {
+            parameters: bindings.iter().map(|(id, _)| *id).collect(),
+            body: Box::new(body),
         }),
-    }
+        args: bindings.iter().map(|(_, value)| value.clone()).collect(),
+    };
+    Ok((rest, generated_exprc))
 }
 
-fn parse_binding<'a>(tokens: &Vec<&'a lexpr::Value>) -> (&'a str, ExprC<'a>) {
-    match &tokens[..] {
-        [lexpr::Value::Symbol(from), lexpr::Value::Symbol(equals), to] if &**equals == "=" => {
-            (from, parse(to))
-        }
-        _ => panic!("`parse_binding` incorrect syntax for binding {:?}", tokens),
-    }
+fn parse_binding(input: &str) -> IResult<&str, (&str, ExprC)> {
+    let (rest, (_, id, _, value, _)) = tuple((
+        char('('),
+        delimited(multispace0, parse_legal_id, multispace0),
+        char('='),
+        delimited(multispace0, parse_exprc, multispace0),
+        char(')'),
+    ))(input)?;
+    Ok((rest, (id, value)))
+}
+
+fn parse_cloc(input: &str) -> IResult<&str, ExprC> {
+    let (rest, (_, parameters, _, _, _, body, _)) = tuple((
+        tag("("),
+        many0(delimited(multispace0, parse_legal_id, multispace0)),
+        multispace0,
+        tag("=>"),
+        multispace0,
+        parse_exprc,
+        tag(")"),
+    ))(input)?;
+    Ok((
+        rest,
+        ExprC::CloC {
+            parameters,
+            body: Box::new(body),
+        },
+    ))
 }
 
 fn environment_lookup<'a, 'b>(id: &str, env: &'b Environment<'a>) -> Value<'a> {
     match env.borrow() {
-        Cons::Some { value: BindingV { from, to }, next:_ } if from == &id => {
-            to.clone()
-        }
-        Cons::Some { value:_, next } => environment_lookup(id, next),
-        Cons::Empty => panic!("JILI unbound identifier: {}", id)
+        Cons::Some {
+            value: BindingV { from, to },
+            next: _,
+        } if from == &id => to.clone(),
+        Cons::Some { value: _, next } => environment_lookup(id, next),
+        Cons::Empty => panic!("JILI unbound identifier: {}", id),
     }
 }
 
@@ -253,9 +298,9 @@ fn interp<'a, 'b>(exp: &'a ExprC<'a>, env: &'b Environment<'a>) -> Value<'a> {
                     let next_env = clo_env.clone();
 
                     let extension = parameters
-                    .iter()
-                    .zip(args.iter().map(|arg| interp(arg, env)))
-                    .map(|(from, to)| BindingV { from, to });
+                        .iter()
+                        .zip(args.iter().map(|arg| interp(arg, env)))
+                        .map(|(from, to)| BindingV { from, to });
                     let extended_env = next_env.extend(extension);
 
                     interp(body, &extended_env)
@@ -312,8 +357,8 @@ fn jili_error<'a>(value: Value) -> Value<'a> {
 }
 
 pub fn top_interp(source: &str) -> String {
-    let sexp = lexpr::from_str(source).unwrap();
-    let prog = parse(&sexp);
+    // let sexp = lexpr::from_str(source).unwrap();
+    let (_, prog) = parse_exprc(source).unwrap();
 
     let base_env: Environment = Cons::from_vec(vec![
         jili_binary_arith!(+),
@@ -323,9 +368,7 @@ pub fn top_interp(source: &str) -> String {
         jili_binary_arith!(<=),
         BindingV {
             from: "equal?",
-            to: Value::Intrinsic(Intrinsic::Binary(|a, b| {
-                Value::BoolV(a == b)
-            })),
+            to: Value::Intrinsic(Intrinsic::Binary(|a, b| Value::BoolV(a == b))),
         },
         BindingV {
             from: "error",
@@ -360,7 +403,7 @@ mod tests {
             "11",
             "Binary operation program"
         );
-        assert_eq!(top_interp("((=> 7))"), "7", "No argument lambda");
+        assert_eq!(top_interp("(( => 7))"), "7", "No argument lambda");
         assert_eq!(
             top_interp("(((x => (x => (+ 3 x)))5)7)"),
             "10",
@@ -368,18 +411,6 @@ mod tests {
         );
         assert_eq!(top_interp("(((=> +)) 3 4)"), "7", "return intrinsic");
         assert_eq!(top_interp("(var ((x = 5)) in x)"), "5", "Basic Binding");
-        assert_eq!(
-            top_interp(
-                "(var (
-                    (hi = 5)
-                    (bye = (+ 2 hi))
-                    (f = (=> (+ hi bye)))
-                    ) in
-                    (f))"
-            ),
-            "12",
-            "More complex binding"
-        );
         assert_eq!(
             top_interp(
                 "(var (
@@ -418,5 +449,41 @@ mod tests {
     #[should_panic(expected = "JILI user-error got: Hello")]
     fn error_causes_panic() {
         top_interp(r#"(error "Hello")"#);
+    }
+
+    #[test]
+    fn parse_tests() {
+        assert_eq!(
+            parse_cloc("(x => 5)").unwrap().1,
+            ExprC::CloC {
+                parameters: vec!["x"],
+                body: Box::new(ExprC::NumC(5.))
+            }
+        );
+        assert_eq!(
+            parse_cloc("(x y => 5)").unwrap().1,
+            ExprC::CloC {
+                parameters: vec!["x", "y"],
+                body: Box::new(ExprC::NumC(5.))
+            }
+        );
+        assert_eq!(
+            parse_cloc("(x y => (+ x y))").unwrap().1,
+            ExprC::CloC {
+                parameters: vec!["x", "y"],
+                body: Box::new(ExprC::AppC {
+                    fun: Box::new(ExprC::IdC("+")),
+                    args: vec![ExprC::IdC("x"), ExprC::IdC("y")]
+                })
+            }
+        );
+        assert_eq!(
+            parse_appc("(+ 5 10)").unwrap().1,
+            ExprC::AppC {
+                fun: Box::new(ExprC::IdC("+")),
+                args: vec![ExprC::NumC(5.), ExprC::NumC(10.)]
+            }
+        );
+        assert!(parse_appc("((x => (+ x 10)) 1)").is_ok());
     }
 }
